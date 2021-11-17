09242d1550a7561d6c5dcc3a416ce2cffe76e641
Aldren <aldrenbobiles@gmail.com>
2021-11-18 00:57:32 +0800

    added junit for file loading

---
+++ src/test/java/com/aldren/input/service/impl/FileInputServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.aldren.input.service.impl;
+
+import com.aldren.input.service.InputService;
+import com.aldren.properties.FileInputProperties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
+@EnableConfigurationProperties(value = FileInputProperties.class)
+@ActiveProfiles("test")
+public class FileInputServiceImplTest {
+
+    @Autowired
+    private FileInputProperties fileInputProperties;
+
+    private InputService inputService;
+
+    @BeforeEach
+    public void init() {
+        inputService = new FileInputServiceImpl(fileInputProperties);
+    }
+
+    @Test
+    public void testProcessInput() {
+        int expected = 1;
+        List<String> lines = inputService.processInput();
+
+        assertEquals(expected, lines.size());
+    }
+
+}
