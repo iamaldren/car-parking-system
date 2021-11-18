63c751d7e5ab4b9fb2d37ffb36c6f6be044c39d9
Aldren <aldrenbobiles@gmail.com>
2021-11-18 09:40:49 +0800

    service events

---
+++ src/main/java/com/aldren/event/service/EventService.java
@@ -0,0 +1,7 @@
+package com.aldren.event.service;
+
+public interface EventService {
+
+    void processEvent(String event);
+
+}
---
+++ src/main/java/com/aldren/event/service/impl/ExitEventService.java
@@ -0,0 +1,14 @@
+package com.aldren.event.service.impl;
+
+import com.aldren.event.service.EventService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExitEventService implements EventService {
+
+    @Override
+    public void processEvent(String event) {
+
+    }
+
+}
